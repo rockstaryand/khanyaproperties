39c05b2669ad2899a805855e9250b47290fae2d7
Yandisa <yandi@LAPTOP-74A1OF39>
2023-02-14 17:31:26 +0200

    "new updates"

--- validate.js
+++ validate.js
@@ -1,20 +1,6 @@
-const form = document.getElementById("contactForm");
 const year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  if (!email || !message) {
-    alert("Please fill out all required fields");
-    return;
-  }
-
-  // add additional email validation logic here
-
-  alert("Form submitted successfully!");
-});
-
+const dob = document.getElementById("dob");
+dob.textContent = new Date().getFullYear() - 1996;
+console.log(dob, "is this the right age?");
